80d5da016d056f6ab18d96bcb8f2df72519896e1
zhikh <zhikh.k@gmail.com>
2024-07-19 20:46:05 +0300

    feat: add basic functions
    Add functions:
    - Exec for queries without result;
    - Get for queries with no more than one result;
    - Select for queries with more than one results.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zhikh23/pg-utils
 
 go 1.22.5
+
+require github.com/jmoiron/sqlx v1.4.0
---
+++ pgutils.go
@@ -0,0 +1,72 @@
+package pgutils
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func sqlErr(err error, query string, args ...interface{}) error {
+	return fmt.Errorf(`run query "%s" with args %+v: %w`, query, args, err)
+}
+
+// Exec does not returns result from query.
+func Exec(ctx context.Context, db sqlx.ExecerContext, query string, args ...interface{}) (sql.Result, error) {
+	res, err := db.ExecContext(ctx, query, args...)
+	if err != nil {
+		return res, sqlErr(err, query, args...)
+	}
+
+	return res, nil
+}
+
+// Get returns no more than one result from query.
+func Get(ctx context.Context, db sqlx.QueryerContext, dest interface{}, query string, args ...interface{}) error {
+	if err := sqlx.GetContext(ctx, db, dest, query, args...); err != nil {
+		return sqlErr(err, query, args...)
+	}
+
+	return nil
+}
+
+// Select returns multiple results from query.
+func Select(ctx context.Context, db sqlx.QueryerContext, dest interface{}, query string, args ...interface{}) error {
+	if err := sqlx.SelectContext(ctx, db, dest, query, args...); err != nil {
+		return sqlErr(err, query, args...)
+	}
+
+	return nil
+}
+
+type TxFunc func(tx *sqlx.Tx) error
+
+type TxRunner interface {
+	BeginTxx(context.Context, *sql.TxOptions) (*sqlx.Tx, error)
+}
+
+// RunTx wraps the function into a transaction.
+// If f returns error, RunTx will roll back transaction.
+func RunTx(ctx context.Context, db TxRunner, f TxFunc) (err error) {
+	var tx *sqlx.Tx
+
+	opts := &sql.TxOptions{
+		Isolation: sql.LevelReadCommitted,
+	}
+
+	tx, err = db.BeginTxx(ctx, opts)
+	if err != nil {
+		return fmt.Errorf("failed to begin transaction: %w", err)
+	}
+	defer func() {
+		if err != nil {
+			err = errors.Join(err, tx.Rollback())
+		} else {
+			err = tx.Commit()
+		}
+	}()
+
+	return f(tx)
+}
